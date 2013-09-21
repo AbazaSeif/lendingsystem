bc0307d540bbcded0658d81905e861a7ed179182
JC Ricaro <jeycir@gmail.com>
2013-09-21 21:13:43 +0800

    Loans handler

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -485,8 +485,8 @@ public function inbox() {
 
 		if (1 == 1) {// Call the SOAP method, note the definition of the xmlnamespace as the third parameter in the call and how the posted message is added to the message string
 			$result = $client->call('sendSMS', array(
-				'uName' => '4pcf414hq',
-				'uPin' => '21738474',
+				'uName' => 'so9mcfhyp',
+				'uPin' => '21737147',
 				'MSISDN' => '0'.substr($outgoing->number,-10),
 				'messageString' => $outgoing->message,
 				'Display' => '1', // 1 for normal message
