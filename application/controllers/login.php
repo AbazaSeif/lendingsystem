1e188f7bfb0483e1a723553f8bc33281fb775f99
JC Ricaro <jeycir@gmail.com>
2013-09-21 20:24:44 +0800

    Loans handler

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -118,6 +118,28 @@ class Login extends CI_Controller {
 		echo 'Loans handler.';
 	}
 
+function gen_uuid() {
+        return sprintf('%04x',
+            // 32 bits for "time_low"
+            mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+                
+            // 16 bits for "time_mid"
+            mt_rand( 0, 0xffff ),
+                
+            // 16 bits for "time_hi_and_version",
+            // four most significant bits holds version number 4
+            mt_rand( 0, 0x0fff ) | 0x4000,
+                    
+            // 16 bits, 8 bits for "clk_seq_hi_res",
+            // 8 bits for "clk_seq_low",
+            // two most significant bits holds zero and one for variant DCE1.1
+            mt_rand( 0, 0x3fff ) | 0x8000,
+                    
+            // 48 bits for "node"
+            mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+            );
+    } 
+
 	function messagehandler() {
 		$this->load->model('sms_model');
 		/* change behavior depending on the type of request. If we receive a POST request, then parse the XML document embedded within and save to the database. We can add additional validation logic later */
