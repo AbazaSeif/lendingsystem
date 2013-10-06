48398bbf2e40f1067c24f4453013abe6325df08f
JC Ricaro <jeycir@gmail.com>
2013-10-06 23:58:58 +0800

    Forgot Password

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -125,13 +125,15 @@ class Login extends CI_Controller {
 			$hash = $this->gen_hash();
 			$user = $this->users_model->get_user($this->input->post('email'));
 
-			$this->load->library('email');
+			// $this->load->library('email');
 
-			$this->email->from('noreply@lending-system.gopagoda.com','Lending System');
-			$this->email->to($this->input->post('email'));
-			$this->email->subject('Lending system Password reset');
-			$this->email->message("To reset you're password, please go to this link: ".base_url('/login/resetpassword/'.$hash));
-			$this->email->send();
+			// $this->email->from('noreply@lending-system.gopagoda.com','Lending System');
+			// $this->email->to($this->input->post('email'));
+			// $this->email->subject('Lending system Password reset');
+			
+			// $this->email->send();
+
+			mail($this->input->post('email'), 'Lending system Password reset',"To reset you're password, please go to this link: ".base_url('/login/resetpassword/'.$hash));
 
 			$db = array(
 				'user_id' => $user->id,
